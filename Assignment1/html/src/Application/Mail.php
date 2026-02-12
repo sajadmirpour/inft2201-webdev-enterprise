00e4ce40ca9355374da144c3e86ee6c8856d0911
sajadmirpour <sajad.m0526@gmail.com>
2026-02-11 23:26:00 -0500

    Updated Mail.php to match mail and correspond with MailTest.php

--- Assignment1/html/src/Application/Mail.php
+++ Assignment1/html/src/Application/Mail.php
@@ -10,11 +10,39 @@ class Mail {
     {
         $this->pdo = $pdo;
     }
-
+    // Create mail
     public function createMail($subject, $body) {
         $stmt = $this->pdo->prepare("INSERT INTO mail (subject, body) VALUES (?, ?) RETURNING id");
         $stmt->execute([$subject, $body]);
 
         return $stmt->fetchColumn();
+    }
+    // Get mail by ID
+    public function getMail($id) {
+        $stmt = $this->pdo->prepare("SELECT * FROM mail WHERE id = ?");
+        $stmt->execute([$id]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result ?: false;
+    }
+    // Get all mail
+    public function getAllMail() {
+        $stmt = $this->pdo->query("SELECT * FROM mail");
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    // Update mail
+    public function updateMail($id, $subject, $body) {
+        $stmt = $this->pdo->prepare("UPDATE mail SET subject = ?, body = ? WHERE id = ?");
+        $stmt->execute([$subject, $body, $id]);
+
+        return $stmt->rowCount() > 0;
+    }
+    // Delete mail
+    public function deleteMail($id) {
+        $stmt = $this->pdo->prepare("DELETE FROM mail WHERE id = ?");
+        $stmt->execute([$id]);
+
+        return $stmt->rowCount() > 0;
     }
 }
