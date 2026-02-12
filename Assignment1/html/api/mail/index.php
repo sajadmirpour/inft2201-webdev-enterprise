5e73d1659441c49f6f5b1c581b3df3d9a3c3795e
sajadmirpour <sajad.m0526@gmail.com>
2026-02-11 23:29:10 -0500

    Updated mail/index.php

--- Assignment1/html/api/mail/index.php
+++ Assignment1/html/api/mail/index.php
@@ -18,11 +18,24 @@ try {
 $mail = new Mail($pdo);
 $page = new Page();
 
+// List all mail
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $page->list($mail->getAllMail());
+    exit;  
+}
+// Create mail
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $json = file_get_contents("php://input");
     $data = json_decode($json, true);
-
-    $page->item($mail->createMail($data['subject'], $data['body']));
+    //input validation
+    if (!isset($data['subject']) || !isset($data['body'])) {
+        $page->badRequest();
+        exit;
+    }
+    // create mail and return id
+    $id = $mail->createMail($data['subject'], $data['body']);
+    http_response_code(201);
+    echo json_encode(["id" => $id]);
     exit;
 }
 
